60497c1deb5b55a43c5b8569a5acb4960beeaa7d
Ethan Robotham <fireybull@gmail.com>
2024-07-09 12:41:14 -0400

    git init
    Initialization for UART communication logic, send and receive

---
+++ Debug/Example_2806xSci_Echoback.d
@@ -0,0 +1,141 @@
+# FIXED
+
+Example_2806xSci_Echoback.obj: ../Example_2806xSci_Echoback.c
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/DSP28x_Project.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_Cla_typedefs.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Device.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Adc.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_BootVars.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Cla.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Comp.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_CpuTimers.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_DevEmu.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Dma.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_ECan.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_ECap.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_EPwm.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_EQep.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Gpio.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_HRCap.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_I2c.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Mcbsp.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_PieCtrl.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_PieVect.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Spi.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Sci.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_SysCtrl.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Usb.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_XIntrupt.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_Examples.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_GlobalPrototypes.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_EPwm_defines.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_I2c_defines.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_Dma_defines.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_Cla_defines.h
+Example_2806xSci_Echoback.obj: C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_DefaultISR.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdlib.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_ti_config.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/linkage.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdlibf.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/cdefs.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/math.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_defs.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_types.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_types.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_limits.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdio.h
+Example_2806xSci_Echoback.obj: C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdarg.h
+
+../Example_2806xSci_Echoback.c:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/DSP28x_Project.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_Cla_typedefs.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Device.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Adc.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_BootVars.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Cla.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Comp.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_CpuTimers.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_DevEmu.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Dma.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_ECan.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_ECap.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_EPwm.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_EQep.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Gpio.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_HRCap.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_I2c.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Mcbsp.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_PieCtrl.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_PieVect.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Spi.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Sci.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_SysCtrl.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_Usb.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/headers/include/F2806x_XIntrupt.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_Examples.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_GlobalPrototypes.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_EPwm_defines.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_I2c_defines.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_Dma_defines.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_Cla_defines.h:
+
+C:/ti/c2000/C2000Ware_5_02_00_00/device_support/f2806x/common/include/F2806x_DefaultISR.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdlib.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdlibf.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/math.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/_defs.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/machine/_limits.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdio.h:
+
+C:/ti/ccs1271/ccs/tools/compiler/ti-cgt-c2000_22.6.1.LTS/include/stdarg.h:
+
---
+++ Example_2806xSci_Echoback.c
@@ -0,0 +1,293 @@
+//
+// Included Files
+//
+#include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+//
+// Function Prototypes
+//
+void scia_echoback_init(void);
+void scia_fifo_init(void);
+void scia_xmit(int a);
+void scia_msg(char *msg);
+int populate_variable(const char *arr);
+
+//
+// Globals
+//
+Uint16 LoopCount;
+Uint16 ErrorCount;
+
+#define MAX_BUFFER_SIZE 256
+
+//Struct to hold parameters
+typedef struct Parameters
+{
+    int pwmFreq;
+    int testCase;
+} Parameters;
+
+//
+// Main
+//
+void main(void)
+{
+    Uint16 ReceivedChar;
+    char buffer[MAX_BUFFER_SIZE] = { '\0' };
+    Uint16 bufferIndex = 0;
+    char *msg;
+    Parameters ParametersTest;
+
+    //
+    // Step 1. Initialize System Control:
+    // PLL, WatchDog, enable Peripheral Clocks
+    // This example function is found in the F2806x_SysCtrl.c file.
+    //
+    InitSysCtrl();
+
+    //
+    // Step 2. Initalize GPIO:
+    // This example function is found in the F2806x_Gpio.c file and
+    // illustrates how to set the GPIO to its default state.
+    //
+    //InitGpio(); Skipped for this example
+
+    //
+    // For this example, only init the pins for the SCI-A port.
+    // This function is found in the F2806x_Sci.c file.
+    //
+    InitSciaGpio();
+
+    //
+    // Step 3. Clear all interrupts and initialize PIE vector table:
+    // Disable CPU interrupts
+    //
+    DINT;
+
+    //
+    // Initialize PIE control registers to their default state.
+    // The default state is all PIE interrupts disabled and flags
+    // are cleared.
+    // This function is found in the F2806x_PieCtrl.c file.
+    //
+    InitPieCtrl();
+
+    //
+    // Disable CPU interrupts and clear all CPU interrupt flags
+    //
+    IER = 0x0000;
+    IFR = 0x0000;
+
+    //
+    // Initialize the PIE vector table with pointers to the shell Interrupt
+    // Service Routines (ISR).
+    // This will populate the entire table, even if the interrupt
+    // is not used in this example.  This is useful for debug purposes.
+    // The shell ISR routines are found in F2806x_DefaultIsr.c.
+    // This function is found in F2806x_PieVect.c.
+    //
+    InitPieVectTable();
+
+    //
+    // Step 4. Initialize all the Device Peripherals:
+    // This function is found in F2806x_InitPeripherals.c
+    //
+    //InitPeripherals(); // Not required for this example
+
+    //
+    // Step 5. User specific code
+    //
+    LoopCount = 0;
+    ErrorCount = 0;
+
+    scia_fifo_init();     // Initialize the SCI FIFO
+    scia_echoback_init();  // Initalize SCI for echoback
+
+    msg =
+            "\r\nYou will enter a string please press F then input a number (1-5000), and the DSP will echo it back when you press enter!\0";
+    scia_msg(msg);
+
+    for (;;)
+    {
+        //
+        // Wait for inc character
+        //
+        while (SciaRegs.SCIFFRX.bit.RXFFST < 1)
+        {
+            //
+            // wait for a character
+            //
+        }
+
+        //
+        // Get character
+        //
+        ReceivedChar = SciaRegs.SCIRXBUF.all;
+
+        //
+        // Check if it's a terminating character
+        //
+        if (ReceivedChar == '\0')
+        {
+            //
+            // Echo the buffer back
+            //
+            msg = "\r\nYou sent: ";
+            scia_msg(msg);
+            buffer[bufferIndex] = '\0'; // Null-terminate the buffer
+            scia_msg(buffer);
+
+            //TODO: make this a function in the future
+            int i = 0;
+
+            while (buffer[i] != '\0') //loops until null character is found   TODO:change to find length and loop a discrete amount
+            {
+                switch (buffer[i])
+                {
+                case 'f':
+                case 'F':
+                    ParametersTest.pwmFreq = populate_variable(&(buffer[i])); //passes where you left off in the array
+                    i += (int) log10((double) ParametersTest.pwmFreq) + 3; //adds 1 for space, 1 for the letter, and 1 for log, can optimize later
+                    break;
+
+                default:
+                    msg = "\r\nYou sent an invalid character: ";
+                    scia_msg(msg);
+                    scia_xmit(buffer[i]); //prints invalid character to serial monitor
+                    i++;
+                    break;
+                }
+
+                msg = "\r\nthe number you tried to send was: ";
+                scia_msg(msg);
+                sprintf(msg, "%d%c", ParametersTest.pwmFreq, '\0'); //changed int to string and adds terminating character
+                scia_msg(msg);
+            }
+
+            bufferIndex = 0; // Reset buffer index
+            //reset buffer
+            int k = 0;
+            for (k = 0; k < sizeof(buffer); k++)
+            {
+                buffer[k] = '\0';
+            }
+        }
+        else
+        {
+            //
+            // Add character to buffer if there's space
+            //
+            if (bufferIndex < MAX_BUFFER_SIZE - 1)
+            {
+                buffer[bufferIndex++] = ReceivedChar;
+            }
+            else
+            {
+                msg = "To many characters added";
+                scia_msg(msg);
+            }
+        }
+
+        LoopCount++;
+    }
+}
+
+//this function takes in where we left off in the array, and returns an int value
+int populate_variable(const char *arr)
+{
+    char temp[6] = { '\0' };
+    int i = 1; //goes to next spot in array (we were looking at the letter before)
+    int j = 0;
+
+//checks for space between letter and value, ex F 10000
+    if (arr[i] == ' ')
+        i++;
+
+    while (arr[i] != '\0' && arr[i] >= '0' && arr[i] <= '9') //check to make sure array is a number
+    {
+        temp[j++] = arr[i];   //populates temp array to hold value
+        i++;                //keep track of where we are in the array
+    }
+
+    temp[j] = '\0';     // Null-terminate the temp string
+    return atoi(temp); //returns int (converted string to int)
+}
+
+//
+// scia_echoback_init - Test 1,SCIA  DLB, 8-bit word, baud rate 0x0103, 
+// default, 1 STOP bit, no parity
+//
+void scia_echoback_init()
+{
+//
+// Note: Clocks were turned on to the SCIA peripheral
+// in the InitSysCtrl() function
+//
+
+//
+// 1 stop bit,  No loopback, No parity,8 char bits, async mode,
+// idle-line protocol
+//
+    SciaRegs.SCICCR.all = 0x0007;
+
+//
+// enable TX, RX, internal SCICLK, Disable RX ERR, SLEEP, TXWAKE
+//
+    SciaRegs.SCICTL1.all = 0x0003;
+
+    SciaRegs.SCICTL2.bit.TXINTENA = 0;
+    SciaRegs.SCICTL2.bit.RXBKINTENA = 0;
+
+//
+// 9600 baud @LSPCLK = 22.5MHz (90 MHz SYSCLK)
+//
+    SciaRegs.SCIHBAUD = 0x0001;
+    SciaRegs.SCILBAUD = 0x0024;
+
+    SciaRegs.SCICTL1.all = 0x0023;  // Relinquish SCI from Reset
+}
+
+//
+// scia_xmit - Transmit a character from the SCI
+//
+void scia_xmit(int a)
+{
+    while (SciaRegs.SCIFFTX.bit.TXFFST != 0)
+    {
+        // Wait for the transmit buffer to be empty
+
+    }
+    SciaRegs.SCITXBUF = a; //placed below to ensure that the transmit buffer is empty before attempting to send data
+}
+
+//
+// scia_msg - 
+//
+void scia_msg(char *msg)
+{
+    int i;
+    i = 0;
+    while (msg[i] != '\0') //keeps transmitting data until end of line (enter has been pressed, denoted by '\0')
+    {
+        scia_xmit(msg[i]); //turns the letter to be represented by a number (ascii)
+        i++; //increases to next element in the array
+    }
+}
+
+//
+// scia_fifo_init - Initalize the SCI FIFO
+//
+void scia_fifo_init()
+{
+
+    SciaRegs.SCIFFTX.all = 0xE040;
+    SciaRegs.SCIFFRX.all = 0x2044;
+    SciaRegs.SCIFFCT.all = 0x0;
+}
+
+//
+// End of File
+//
